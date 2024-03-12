6eb43c4b015fd0b19c1fa504b6cd8f105b4dbda5
tuhinhossain123 <csetuhin55@gmail.com>
2024-03-12 23:20:19 +0600

    code change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-center">Welcome Everyone</h1>
-      <h1 className="text-center">Welcome  To Mone Transction App</h1>
+      <h1 className="text-center">Welcome  To Money Transction App</h1>
     </main>
   );
 }
