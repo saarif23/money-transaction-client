9b4845449b70abbc72959f589229e0b216586e55
tahmidurrahmann <138325180+tahmidurrahmann@users.noreply.github.com>
2024-03-14 15:42:59 +0600

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-center">Welcome Everyone</h1>
-      <h1 className="text-center">Welcome Everyone</h1>
-      <h1 className="text-center">Welcome Everyone</h1>
     </main>
   );
 }
