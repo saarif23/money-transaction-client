44b1b2b83c963e887dd38a425539b126287ccda8
tahmidurrahmann <138325180+tahmidurrahmann@users.noreply.github.com>
2024-03-15 22:20:29 +0600

    added edited home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,14 @@
 import DownloadApp from "@/Components/Home/DownloadApp/DownloadApp";
 import TransactionCategories from "@/Components/Home/TransactionCategories/TransactionCategories";
-import Image from "next/image";
-
 import WhyChooseUs from "@/Components/Home/WhyChooseUs/WhyChooseUs";
 import Navbar from "@/Components/shared/Navbar/Navbar";
-
 
 export default function Home() {
   return (
     <main>
+      <Navbar />
      <DownloadApp></DownloadApp>
      <TransactionCategories></TransactionCategories>
-      <Navbar />
-      <DownloadApp></DownloadApp>
       <WhyChooseUs />
     </main>
   );
