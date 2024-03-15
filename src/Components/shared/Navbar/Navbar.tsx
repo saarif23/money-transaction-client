cbef88169c99da9e8b6113bdb8900f041e8f2fc6
Arif Hossain <arif23s.001@gmail.com>
2024-03-16 01:44:55 +0600

    Update Navbar.tsx

--- src/Components/shared/Navbar/Navbar.tsx
+++ src/Components/shared/Navbar/Navbar.tsx
@@ -1,30 +1,17 @@
-"use client";
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-
-import {
-  Button,
-  Divider,
-  Drawer,
-  IconButton,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemText,
-  Stack,
-  Typography,
-} from "@mui/material";
-
+"use client"
+import React, { useState } from "react";
+import { AppBar, Box, Container, Drawer, IconButton, List, ListItem, ListItemButton, ListItemText, Stack, Typography, Button, Divider } from "@mui/material";
 import Link from "next/link";
-
-//drawer
-import { MdMenu } from "react-icons/md";
-import { MdClose } from "react-icons/md";
+import { MdMenu, MdClose } from "react-icons/md";
 
 const drawerWidth = 240;
-const navItems = [
+
+interface NavItem {
+  route: string;
+  pathName: string;
+}
+
+const navItems: NavItem[] = [
   {
     route: "Home",
     pathName: "/",
@@ -44,34 +31,26 @@ const navItems = [
 ];
 
 function Navbar() {
-  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [mobileOpen, setMobileOpen] = useState(false);
+
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
   };
+
   const drawer = (
-    <Box
-      onClick={handleDrawerToggle}
-      sx={{ textAlign: "center", marginTop: "20px" }}
-    >
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center", marginTop: "20px" }}>
       <Divider />
       <List>
         {navItems.map((item, idx) => (
           <Link key={idx} href={item.pathName}>
-            <ListItem
-              disablePadding
-              sx={{
-                "& :hover": {
-                  color: "red",
-                  fontWeight: "800",
-                  textDecoration: "underline",
-                },
-              }}
-            >
-              <ListItemButton
-                sx={{
-                  textAlign: "center",
-                }}
-              >
+            <ListItem disablePadding sx={{
+              "& :hover": {
+                color: "red",
+                fontWeight: "800",
+                textDecoration: "underline",
+              },
+            }}>
+              <ListItemButton sx={{ textAlign: "center" }}>
                 <ListItemText primary={item.route} />
               </ListItemButton>
             </ListItem>
@@ -83,74 +62,49 @@ function Navbar() {
 
   return (
     <>
-      <AppBar
-        position="fixed"
-        sx={{
-          backgroundColor: "#009286",
-        }}
-      >
-        <Container
-        
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            padding: "10px 5px",
-          }}
-        >
+      <AppBar position="fixed" sx={{ backgroundColor: "#009286" }}>
+        <Container sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding: "10px 5px",
+        }}>
           <Typography variant="h5" color="white">
             Navbar
           </Typography>
-          <Box
-            sx={{
-              "& button:hover": {
-                color: "#90006F",
-                textDecoration: "underline",
-              },
-              display: { xs: "none", md: "block" },
-            }}
-          >
-            {navItems.map((item) => (
-              <Link key={item} href={item.pathName}>
+          <Box sx={{
+            "& button:hover": {
+              color: "#90006F",
+              textDecoration: "underline",
+            },
+            display: { xs: "none", md: "block" },
+          }}>
+            {navItems.map((item, idx) => (
+              <Link key={idx} href={item.pathName}>
                 <Button sx={{ color: "white" }}>{item.route}</Button>
               </Link>
             ))}
           </Box>
           <div className="max-lg:block lg:hidden">
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              edge="start"
-              onClick={handleDrawerToggle}
-              sx={{ mr: 2 }}
-            >
+            <IconButton color="inherit" aria-label="open drawer" edge="start" onClick={handleDrawerToggle} sx={{ mr: 2 }}>
               {mobileOpen ? <MdClose /> : <MdMenu />}
             </IconButton>
           </div>
         </Container>
       </AppBar>
       <nav>
-        <Drawer
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true,
-          }}
-          sx={{
-            display: { xs: "block", md: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-        >
+        <Drawer variant="temporary" open={mobileOpen} onClose={handleDrawerToggle} ModalProps={{ keepMounted: true }} sx={{
+          display: { xs: "block", md: "none" },
+          "& .MuiDrawer-paper": {
+            boxSizing: "border-box",
+            width: drawerWidth,
+          },
+        }}>
           {drawer}
         </Drawer>
       </nav>
-      
     </>
   );
 }
-export default Navbar;
 
+export default Navbar;
