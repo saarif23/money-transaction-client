4b76f7b01b0d61c24ef22398d597633d0015dd13
tahmidurrahmann <138325180+tahmidurrahmann@users.noreply.github.com>
2024-03-15 22:43:02 +0600

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import DownloadApp from "@/Components/Home/DownloadApp/DownloadApp";
 import TransactionCategories from "@/Components/Home/TransactionCategories/TransactionCategories";
 import WhyChooseUs from "@/Components/Home/WhyChooseUs/WhyChooseUs";
-import Navbar from "@/Components/shared/Navbar/Navbar";
 
 export default function Home() {
   return (
