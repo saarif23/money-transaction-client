cc0b411c13eb3e9f98026cd40ab3a8255d9ca700
saarif23 <arif23s.001@gmail.com>
2024-03-15 09:30:56 +0600

    create offers  folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,6 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-center">Welcome Everyone</h1>
-      <h1 className="text-center">Welcome Everyone</h1>
-      <h1 className="text-center">Welcome Everyone</h1>
     </main>
   );
 }
