84c5932ef6ea8ceac287cb5d21f25c60d78ae315
Arif Hossain <arif23s.001@gmail.com>
2024-03-17 23:13:29 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,32 +3,14 @@ import Homes from "@/Components/Home/Homes";
 import TransactionCategories from "@/Components/Home/TransactionCategories/TransactionCategories";
 import WhyChooseUs from "@/Components/Home/WhyChooseUs/WhyChooseUs";
 import Footer from "@/Components/Home/Footer/Footer"
-export default function Home() {
-  return (
-    <main>
-   
-
-        <Homes /> 
-     
-      <DownloadApp />
-      <TransactionCategories />
-      <WhyChooseUs /> 
-   <div className="mt-96">
-        <Footer />
-   </div>
-
 import AllOffers from "@/Components/Home/Offers/AllOffers";
-
-
 export default function Home() {
   return (
     <main>
-      <TransactionCategories></TransactionCategories>
-      <DownloadApp></DownloadApp>
-      <DownloadApp />
-      <TransactionCategories />
-      <WhyChooseUs />
-      <AllOffers></AllOffers>
+      <TransactionCategories/>
+          <DownloadApp />
+           <WhyChooseUs />
+      <AllOffers/>
     </main>
   );
 }
